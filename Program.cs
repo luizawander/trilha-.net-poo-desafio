8f0d42935286a1b071734527c71077859d5b65b6
Luiza Sales <154068580+luizawander@users.noreply.github.com>
2024-04-27 11:05:40 -0300

    desafio ok

--- Program.cs
+++ Program.cs
@@ -1,3 +1,30 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Nokia nokia = new Nokia("123456789", "Nokia 3310", "IMEI123", 512);
+
+            
+            nokia.Ligar();
+            nokia.ReceberLigacao();
+            nokia.InstalarAplicativo("WhatsApp");
+
+            Console.WriteLine();
+
+            
+            Iphone iphone = new Iphone("987654321", "iPhone 12", "IMEI456", 256);
+
+            
+            iphone.Ligar();
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Instagram");
+
+            Console.ReadLine();
+        }
+    }
+}
